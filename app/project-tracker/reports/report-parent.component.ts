4d38b3cb85dfdf8fa8a786f44505ec7b36f40f22
Eros Niko Alvarez <iamerosniko@yahoo.com>
2017-03-02 08:10:41 -0500

    up

---
+++ app/project-tracker/reports/report-parent.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { UUID } from 'angular2-uuid';
+import { ActivatedRoute,  Params, Router } from '@angular/router';
+//Folder : project
+import { ReportService } from './report.service';
+//Folder : Projects
+import { ProjectService } from '../projects/project.service';
+import { Project } from '../projects/project';
+//Folder : Details
+import { Detail } from '../details/detail';
+@Component({
+    moduleId: module.id,
+    templateUrl:`incident-parent.component.html`
+})
+
+export class ReportParentComponent implements OnInit{
+    project : Project = new Project('','','','','',false,false);
+    detailList : Detail[];
+    projectID : string ='';
+    viewpage : number = 0;
+    isNew : boolean = false;
+    selectedDetail : Detail = new Detail('','','','','','','',null,null,null,null,'awaiting',false,true,false,'',0,'Incident');
+        
+    constructor(
+        public incidentService : IncidentService,
+        private projectService : ProjectService,
+        private route: ActivatedRoute,
+        private router: Router
+    ){ }
+
+    refreshList(): void {
+        this.incidentService.getIncidents(this.projectID).then(detail => this.detailList = detail);
+    }
+
+    getProjectDetail(): void{
+        this.projectService.getProject(this.projectID).then(project => this.project = project);
+    }
+
+    newRecord(): void{
+        this.isNew=true;
+        this.viewpage=1;
+        this.selectedDetail=new Detail(UUID.UUID(),this.projectID,'','','','','',null,null,null,null,'awaiting',false,true,false,'',0,'Incident');
+    }
+    
+    saveRecord(): void{
+        this.isNew ? this.incidentService.postIncident(this.selectedDetail) : this.incidentService.putIncident(this.selectedDetail);
+        this.isNew=false;
+    }
+
+    deleteRecord(detail:Detail): void{
+        if (confirm("Are you sure you want to delete?")) {
+            detail.pt_detail_deleted=true;
+            this.incidentService.putIncident(detail);
+            setTimeout(
+                () => {
+                    this.refreshList();
+                }, 
+                750
+            );
+        }
+    }
+    getselectedDetailID(){
+        this.route.params.subscribe(params => {
+            this.projectID = params['id'];});    
+    }
+
+    ngOnInit(){
+        this.getselectedDetailID();
+        this.refreshList();
+    }
+}
---
+++ app/project-tracker/reports/report.service.ts
@@ -0,0 +1,30 @@
+import 'rxjs/add/operator/toPromise';
+import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+import { UUID } from 'angular2-uuid';
+import { Detail } from '../details/detail';
+
+@Injectable()
+export class ReportService {
+    private headers = new Headers({'Content-Type': 'application/json'});
+    private detailsUrl = 'api/reports';  // testing
+
+    constructor(private http: Http){}
+    
+    getIncident(id: string): Promise<Detail> {
+        const url = `${this.detailsUrl}/GetDetail/?detailID=${id}`;
+
+        return this.http
+                .get(url,{headers: this.headers})
+                .toPromise()
+                .then(response => response.json())  // testing
+                //.then(response => response.json())  // live
+                .catch(this.handleError);
+
+    }
+
+    private handleError(error: any): Promise<any> {
+        console.error('An error occurred', error); // for demo purposes only
+        return Promise.reject(error.message || error);
+    }
+}
